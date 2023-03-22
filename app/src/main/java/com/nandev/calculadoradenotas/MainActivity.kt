5ef9a146e8e0bde35441546888d4c98fb0a1891c
hernanbarrientos <hrn.2211@gmail.com>
2023-03-22 11:08:04 -0300

    Capturate a input with text created the logic of calculations setting the colors in the texts

--- app/src/main/java/com/nandev/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/nandev/calculadoradenotas/MainActivity.kt
@@ -13,5 +13,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener {
+            val num1 = binding.nota1
+            val num2 = binding.nota2
+            val num3 = binding.nota3
+            val num4 = binding.nota4
+            val faltas = binding.faltas
+            val resultado = binding.tvResultado
+
+            val nota1 = Integer.parseInt(num1.text.toString())
+            val nota2 = Integer.parseInt(num2.text.toString())
+            val nota3 = Integer.parseInt(num3.text.toString())
+            val nota4 = Integer.parseInt(num4.text.toString())
+            val numeroFaltas = Integer.parseInt(faltas.text.toString())
+
+            val media = (nota1 + nota2 + nota3 + nota4) / 4
+
+            if(media >=5 && numeroFaltas <=20){
+                resultado.setText("Aluno foi Aprovado \n Média final $media")
+                resultado.setTextColor(getColor(R.color.green))
+            }else if(numeroFaltas >20){
+                resultado.setText("Aluno foi Reprovado por faltas \n Média final $media")
+                resultado.setTextColor(getColor(R.color.red))
+            }else if(media <5){
+                resultado.setText("Aluno foi Reprovado por Nota \n Média final $media")
+                resultado.setTextColor(getColor(R.color.red))
+            }
+
+        }
     }
 }
